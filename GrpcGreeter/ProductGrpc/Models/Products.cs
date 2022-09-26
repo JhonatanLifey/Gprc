a4f67bb3de4ba3aace060f8a56da3417c89fa724
Jhonatan Carranza Conde <jhonatanlifey@outlook.com>
2022-09-26 16:30:22 +0930

    Start Product - Grpc

---
+++ GrpcGreeter/ProductGrpc/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿namespace ProductGrpc.Models
+{
+	public class Products
+	{
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public ProductStatus Status { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+
+    public enum ProductStatus
+    {
+        INSTOCK = 0,
+        LOW = 1,
+        NONE = 2
+    }
+
+}
---
+++ GrpcGreeter/ProductGrpc/Persistence/ProductsContexts.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductGrpc.Models;
+
+namespace ProductGrpc.Persistence
+{
+	public class ProductsContexts : DbContext
+	{
+		public ProductsContexts(DbContextOptions<ProductsContexts> options)
+			: base(options)
+		{
+
+		}
+
+
+		public DbSet<Products> Products {get; set;}
+
+
+	}
+}
